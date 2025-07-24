a0c13bbbaf40a4ef3b281685c105309f1f3f757e
Kalinski <lucascunha@xpert.com.br>
2025-07-24 16:14:33 -0300

    refactor: mudado para pasta domain

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_item";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/order_item";
 
 const customer = new Customer("123", "Wesley");
 const address = new Address("Rua 2", 2, "123", "City");
